c33b0369dbc7497214616c305505ff471d28321a
derfenix <derfenix@gmail.com>
2019-09-11 09:24:47 +0300

    Add pool of statsMessage items, to reduce memory allocations

--- pkg/collector/collector.go
+++ pkg/collector/collector.go
@@ -3,26 +3,35 @@ package collector
 import (
 	"encoding/json"
 	"log"
+	"sync"
 
 	"gopkg.in/olahol/melody.v1"
 )
 
-// StatsMessage struct of message, received from ws client
-type StatsMessage struct {
-	Id    uint64 `json:"id"`
+// statsMessage struct of message, received from ws client
+type statsMessage struct {
+	ID    uint64 `json:"id"`
 	Label string `json:"label"`
 }
 
 // GetMessageHandler returns handler for incoming ws messages
 //
-// Unmarshal incoming message into `StatsMessage` and call `Storage.Add` for received data
+// Unmarshal incoming message into `statsMessage` and call `Storage.Add` for received data
 func GetMessageHandler(storage *Storage) func(*melody.Session, []byte) {
+	pool := sync.Pool{
+		New: func() interface{} { return new(statsMessage) },
+	}
+
 	return func(_ *melody.Session, msg []byte) {
-		message := new(StatsMessage)
+		message := pool.Get().(*statsMessage)
 		if err := json.Unmarshal(msg, message); err != nil {
 			log.Printf("failed to unmarshal incoming message: %s", err.Error())
 			return
 		}
-		storage.Add(message.Id, message.Label, 1)
+		storage.Add(message.ID, message.Label, 1)
+
+		message.ID = 0
+		message.Label = ""
+		pool.Put(message)
 	}
 }
